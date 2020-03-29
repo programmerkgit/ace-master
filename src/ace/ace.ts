9030d565c1d28cd3b4945ec02ea06098d64b1aa5
admin <k.kubo.private.mobile@gmail.com>
2020-03-30 01:45:30 +0900

    refactor ace

--- src/ace/ace.ts
+++ src/ace/ace.ts
@@ -1,3 +1,20 @@
+// require fix-browser
+
+// require dom
+// require event
+
+/**
+ * // The following require()s are for inclusion in the built ace file
+ * require("./worker/worker_client");
+ * require("./keyboard/hash_handler");
+ * require("./placeholder");
+ * require("./multi_select");
+ * require("./mode/folding/fold_mode");
+ * require("./theme/textmate");
+ * require("./ext/error_marker");
+ * exports.config = require("./config");
+ */
+
 class Range {
 }
 
@@ -18,6 +35,20 @@ class UndoManager {
 class Renderer {
 }
 
+/**
+ * Ace has three methods
+ * ace.require
+ * ace.config
+ * ace.createEditSession
+ * ace.edit
+ * Range
+ * Editor
+ * EditorSession
+ * UndoManager
+ * VirtualRenderer => Renderer
+ * version config.version
+ * */
+
 
 class Ace {
     createEditSession(text: Document | string, mode: any): EditSession {
@@ -27,22 +58,33 @@ class Ace {
         return doc;
     }
 
-    edit(element: string, options: any): void
-    edit(element: Element, options: any): void
-    edit(element: Element | string, options: any): void {
-        /* set el */
-        let el: Element | null;
+    static getElementByIdOrThrow(element: string): Element {
+        const el = document.getElementById(element);
+        if (!el)
+            throw new Error('ace.edit can\'t find div #' + element);
+        return el;
+    }
+
+    static isAceElement(el: Element): boolean {
+        return el && (el as any).env && (el as any).env.editor instanceof Editor;
+    }
+
+    /**
+     * element is identifier or element
+     * */
+    edit(element: string, options: any): Editor
+    edit(element: Element, options: any): Editor
+    edit(element: Element | string, options: any): Editor {
+        /* set element */
+        let el: Element;
         if (typeof element === 'string') {
-            /* if element is string, get element by id */
-            el = document.getElementById(element);
-            if (!el)
-                throw new Error('ace.edit can\'t find div #' + element);
+            el = Ace.getElementByIdOrThrow(element);
         } else {
             el = element;
         }
-        /* todo check: it would be Editor's property ?? */
-        if ((el as any).env.editor instanceof Editor) {
-
+        /* todo what is el.env.editor? el.env.editor = Editor */
+        if (Ace.isAceElement(el)) {
+            return (el as any).env.editor;
         }
 
         let value = '';
@@ -68,3 +110,5 @@ class Ace {
         /* return Editor */
     }
 }
+
+export const ace = new Ace();
