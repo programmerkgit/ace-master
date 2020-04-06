21a48934109c55c8a124bdaac150348a27351ff0
admin <k.kubo.private.mobile@gmail.com>
2020-04-07 00:24:05 +0900

    ti

--- src/ace/keyboard/textinput.ts
+++ src/ace/keyboard/textinput.ts
@@ -1,16 +1,51 @@
 /* textarea to be editted */
 
-import { dom } from '../lib/dom';
-import { Editor } from '../editor';
+import {dom} from '../lib/dom';
+import {Editor} from '../editor';
 /* very important line */
 
 // event.addCommandKeyListener(text, host.onCommandKey.bind(host));
 export class TextInput {
     textArea: HTMLTextAreaElement;
+    ignoreFocusEvent: boolean = false;
+    host: Editor;
+    isFocused = false;
+
+    private focus() {
+        this.isFocused = true;
+    }
+
+    private onBlurHost(e: Event) {
+        this.host.onBlur(e);
+    }
+
+    private onFocusHost(e: Event) {
+        this.host.onFocus(e);
+    }
+
+    private setBlurListener() {
+        this.textArea.addEventListener('blur', (e: Event) => {
+            if (!this.ignoreFocusEvent) {
+                this.onBlurHost(e);
+                this.focus();
+            }
+        });
+    }
+
+    private isEdge(): boolean {
+        return false;
+    }
+
+    private setFocusListener(e: Event) {
+        if (!this.ignoreFocusEvent) {
+            this.focus();
+        }
+    }
 
     constructor(parentNode: HTMLElement, host: Editor) {
         const textArea = dom.createElement('textarea') as HTMLTextAreaElement;
         this.textArea = textArea;
+        this.host = host;
         textArea.className = 'ace_text-input';
         textArea.setAttribute('wrap', 'off');
         textArea.setAttribute('autocorrect', 'off');
